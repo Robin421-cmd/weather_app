a7f76cfcbb5e1f05087bba38f25808c718e4ceaf
Robin <82756979+Robin421-cmd@users.noreply.github.com>
2021-08-28 19:05:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 
 include_once("weather_api.html");
 
-?>
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'){ $protocol = "https://".$_SERVER['HTTP_HOST']; } else{ $protocol='http://'.$_SERVER['HTTP_HOST']; }
+
+?>
